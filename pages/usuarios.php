f14b88e89f5e703b22c0de8cbc395a4276496f75
jhenriquez90 <jorge.henriquezg20@gmail.com>
2017-12-19 13:16:18 -0600

    update

--- pages/usuarios.php
+++ pages/usuarios.php
@@ -100,7 +100,9 @@ return $page_links;
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
+        <title>Gestión y Control de Documentos</title>
+        <link rel="shortcut icon" href="../img/logo.png" />
+        <link rel="shortcut icon" href="../img/logo.png" type="image/png" />
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
